3047fef8831f7b28bfe7311041e3dcfaa329d697
nurodev <ben@nuro.dev>
2024-01-14 23:31:16 +0000

    Added typed `import.meta.env` & `Astro.locals` to `env.d.ts`

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,12 @@
+/// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
+
+interface ImportMetaEnv extends Readonly<Record<string, string>> {}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
+
+declare namespace App {
+  interface Locals {}
+}
