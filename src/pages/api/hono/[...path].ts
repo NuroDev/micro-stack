a30928c5052bc0eb653e58e485ebc9dd693ee496
nurodev <ben@nuro.dev>
2024-01-15 00:31:26 +0000

    Added basic Hono example

---
+++ src/pages/api/hono/[...path].ts
@@ -0,0 +1,12 @@
+import { Hono } from 'hono';
+
+import type { APIRoute } from 'astro';
+import { db } from '~/utils/db';
+
+const app = new Hono()
+	.basePath('/api/hono/')
+	.get('/comments', async (c) => c.json({ comments: await db.query.comments.findMany() }));
+
+export type App = typeof app;
+
+export const ALL: APIRoute = (context) => app.fetch(context.request);
