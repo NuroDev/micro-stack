b4f1576bab5ea7130f075f6801fc0f34b523f888
nurodev <ben@nuro.dev>
2024-01-15 00:17:19 +0000

    Updated local SQLite db path

--- src/utils/db.ts
+++ src/utils/db.ts
@@ -1,9 +1,13 @@
 import { drizzle } from 'drizzle-orm/better-sqlite3';
-import Database from 'better-sqlite3';
-import * as schema from '../database/schemas.ts';
 import { migrate } from 'drizzle-orm/better-sqlite3/migrator';
+import SQLite from 'better-sqlite3';
 
-const sqlite = new Database(import.meta.env.PROD ? '/data/db.sqlite3' : './db.sqlite3');
+import * as schema from '~/database/schemas.ts';
+import { name } from '~/../package.json';
+
+const sqlite = new SQLite(
+	import.meta.env.PROD ? `/data/${name}.production.db` : `./${name}.local.db`,
+);
 
 export const db = drizzle(sqlite, { schema });
 
