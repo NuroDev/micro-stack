a02544ed4571da94d4c10db89d3fdc1bf2f2c5e9
nurodev <ben@nuro.dev>
2024-01-17 17:52:13 +0000

    Replace Docker / auth-astro setup with WIP Cloudflare setup

--- astro.config.ts
+++ astro.config.ts
@@ -1,15 +1,19 @@
 import { defineConfig } from 'astro/config';
-
-import auth from 'auth-astro';
-import node from '@astrojs/node';
 import tailwind from '@astrojs/tailwind';
-
 import { getSite } from './src/utils/site';
+
+import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
-	adapter: node({ mode: 'standalone' }),
-	integrations: [auth(), tailwind()],
-	output: 'server',
+	adapter: cloudflare({
+		runtime: {
+			bindings: {},
+			mode: 'local',
+			type: 'pages',
+		},
+	}),
+	integrations: [tailwind()],
 	site: getSite(),
+	output: 'server',
 });
