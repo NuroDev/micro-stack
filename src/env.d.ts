753986654d59f8c7014193da927f1e74d095bfd7
nurodev <ben@nuro.dev>
2024-01-17 18:09:57 +0000

    Updated Astro locals types

--- src/env.d.ts
+++ src/env.d.ts
@@ -7,6 +7,15 @@ interface ImportMeta {
 	readonly env: ImportMetaEnv;
 }
 
+type D1Database = import('@cloudflare/workers-types').D1Database;
+
+interface Bindings {
+	CF_PAGES_URL: string;
+	DATABASE: D1Database;
+}
+
+type Runtime = import('@astrojs/cloudflare').AdvancedRuntime<Bindings>;
+
 declare namespace App {
-	interface Locals {}
+	interface Locals extends Runtime {}
 }
