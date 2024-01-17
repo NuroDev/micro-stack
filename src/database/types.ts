ee8109862d8c0a0d57cfe17865ea3baac1dd1821
nurodev <ben@nuro.dev>
2024-01-17 17:59:35 +0000

    Added missing database types & zod schemas

--- src/database/types.ts
+++ src/database/types.ts
@@ -1,5 +1,18 @@
-import type { InferSelectModel } from 'drizzle-orm';
+import type { InferSelectModel, InferInsertModel } from 'drizzle-orm';
 
-import type { comments } from '~/database/schemas';
+import type { accounts, comments, sessions, users, verificationTokens } from '~/database/schemas';
 
-export type Comment = InferSelectModel<typeof comments>;
+export type SelectAccount = InferSelectModel<typeof accounts>;
+export type InsertAccount = InferInsertModel<typeof accounts>;
+
+export type SelectComment = InferSelectModel<typeof comments>;
+export type InsertComment = InferInsertModel<typeof comments>;
+
+export type SelectSession = InferSelectModel<typeof sessions>;
+export type InsertSession = InferInsertModel<typeof sessions>;
+
+export type SelectUser = InferSelectModel<typeof users>;
+export type InsertUser = InferInsertModel<typeof users>;
+
+export type SelectVerificationToken = InferSelectModel<typeof verificationTokens>;
+export type InsertVerificationToken = InferInsertModel<typeof verificationTokens>;
--- src/database/zod.ts
+++ src/database/zod.ts
@@ -1,6 +1,18 @@
 import { createInsertSchema, createSelectSchema } from 'drizzle-zod';
 
-import { comments } from '~/database/schemas';
+import { accounts, comments, sessions, users, verificationTokens } from '~/database/schemas';
 
-export const InsertCommentSchema = createInsertSchema(comments);
+export const SelectAccountSchema = createSelectSchema(accounts);
+export const InsertAccountSchema = createInsertSchema(accounts);
+
 export const SelectCommentSchema = createSelectSchema(comments);
+export const InsertCommentSchema = createInsertSchema(comments);
+
+export const SelectSessionSchema = createSelectSchema(sessions);
+export const InsertSessionSchema = createInsertSchema(sessions);
+
+export const SelectUserSchema = createSelectSchema(users);
+export const InsertUserSchema = createInsertSchema(users);
+
+export const SelectVerificationTokenSchema = createSelectSchema(verificationTokens);
+export const InsertVerificationTokenSchema = createInsertSchema(verificationTokens);
