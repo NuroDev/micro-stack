ebfd13b679aadb0ac960824cbb9269683757b7e1
nurodev <ben@nuro.dev>
2024-01-17 19:22:56 +0000

    Updated Drizzle config

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -4,11 +4,9 @@ import type { Config } from 'drizzle-kit';
 
 const defineConfig = <T extends Config>(config: T): T => config;
 
-const dbUrl = process.env.NODE_ENV === 'production' ? `/data/app.production.db` : `./app.local.db`;
-
 export default defineConfig({
-	dbCredentials: { url: dbUrl },
-	driver: 'better-sqlite',
+	driver: 'd1',
 	out: './src/database/migrations',
 	schema: './src/database/schemas.ts',
+	verbose: true,
 });
