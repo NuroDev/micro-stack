2f5689ad0b1c07840cff9c35da7efa7c0b51bc28
nurodev <ben@nuro.dev>
2024-01-17 19:23:32 +0000

    Added Cloudflare pages support to `getSite` helper

--- src/utils/site.ts
+++ src/utils/site.ts
@@ -1,6 +1,8 @@
 export function getSite(): string {
 	if (import.meta.env.DEV) return 'http://localhost:4321';
 
+	if (import.meta.env.CF_PAGES_URL) return import.meta.env.CF_PAGES_URL;
+
 	// If one is provided in the environment, use it.
 	if (import.meta.env.SITE) return import.meta.env.SITE;
 
