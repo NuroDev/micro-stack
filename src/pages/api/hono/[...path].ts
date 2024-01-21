a640970af00d588696f126cf93ac36bca8fd4ae2
nurodev <ben@nuro.dev>
2024-01-21 02:20:05 +0000

    Fixed Hono endpoint

--- src/pages/api/hono/[...path].ts
+++ src/pages/api/hono/[...path].ts
@@ -1,12 +1,17 @@
 import { Hono } from 'hono';
 
 import type { APIRoute } from 'astro';
-import { db } from '~/database/client';
 
-const app = new Hono()
+import type { Prettify } from '~/types/utils';
+
+type Bindings = Prettify<App.Locals>;
+
+const app = new Hono<{ Bindings: Bindings }>()
 	.basePath('/api/hono/')
-	.get('/comments', async (c) => c.json({ comments: await db.query.comments.findMany() }));
+	.get('/comments', async (c) =>
+		c.json({ comments: await c.env.drizzle.query.comments.findMany() }),
+	);
 
 export type App = typeof app;
 
-export const ALL: APIRoute = (context) => app.fetch(context.request);
+export const ALL: APIRoute = (context) => app.fetch(context.request, context.locals);
