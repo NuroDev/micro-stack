0dd5671aadb69288316dd5381b4d2399c27f8ab7
nurodev <ben@nuro.dev>
2024-01-21 01:43:45 +0000

    Moved `getSite` to Astro config

--- astro.config.ts
+++ astro.config.ts
@@ -1,8 +1,18 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-import { getSite } from './src/utils/site';
 
 import cloudflare from '@astrojs/cloudflare';
+
+function getSite(): string {
+	if (import.meta.env.DEV) return 'http://localhost:4321';
+
+	if (import.meta.env.CF_PAGES_URL) return import.meta.env.CF_PAGES_URL;
+
+	// If one is provided in the environment, use it.
+	if (import.meta.env.SITE) return import.meta.env.SITE;
+
+	return 'https://example.app';
+}
 
 // https://astro.build/config
 export default defineConfig({
