d713e4c6d31ca0ef5338669ccedddb46406c13a4
nurodev <ben@nuro.dev>
2024-01-21 14:04:04 +0000

    Minor Hono api route refactoring

--- src/pages/api/hono/[...path].ts
+++ src/pages/api/hono/[...path].ts
@@ -1,5 +1,6 @@
 import { Hono } from 'hono';
-import { faker } from '@faker-js/faker/locale/en';
+
+import { commentsHandler } from '~/utils/hono/comments';
 
 import type { APIRoute } from 'astro';
 
@@ -7,30 +8,7 @@ import type { Prettify } from '~/types/utils';
 
 type Bindings = Prettify<App.Locals>;
 
-const app = new Hono<{ Bindings: Bindings }>()
-	.basePath('/api/hono/')
-	.get('/comments', async (c) =>
-		c.json({ comments: await c.env.drizzle.query.comments.findMany() }),
-	)
-	.post('/comments', async (c) => {
-		const rawCount = c.req.query('count');
-		if (!rawCount) return c.json({ ok: false, error: 'Missing count' });
-
-		const count = parseInt(rawCount);
-
-		await Promise.all(
-			new Array(count).fill(null).map(async () => {
-				await c.env.drizzle.insert(c.env.schemas.comments).values({
-					author: faker.person.fullName(),
-					content: faker.lorem.sentence(),
-				});
-
-				await new Promise((resolve) => setTimeout(resolve, Math.random() * 1000));
-			}),
-		);
-
-		return c.json({ ok: true });
-	});
+const app = new Hono<{ Bindings: Bindings }>().basePath('/api/hono/').route('/', commentsHandler);
 
 export type App = typeof app;
 
---
+++ src/utils/hono/comments.ts
@@ -0,0 +1,30 @@
+import { faker } from '@faker-js/faker/locale/en';
+import { Hono } from 'hono';
+
+import type { Prettify } from '~/types/utils';
+
+type Bindings = Prettify<App.Locals>;
+
+export const commentsHandler = new Hono<{ Bindings: Bindings }>()
+	.get('/comments', async (c) =>
+		c.json({ comments: await c.env.drizzle.query.comments.findMany() }),
+	)
+	.post('/comments', async (c) => {
+		const rawCount = c.req.query('count');
+		if (!rawCount) return c.json({ ok: false, error: 'Missing count' });
+
+		const count = parseInt(rawCount);
+
+		await Promise.all(
+			new Array(count).fill(null).map(async () => {
+				await c.env.drizzle.insert(c.env.schemas.comments).values({
+					author: faker.person.fullName(),
+					content: faker.lorem.sentence(),
+				});
+
+				await new Promise((resolve) => setTimeout(resolve, Math.random() * 1000));
+			}),
+		);
+
+		return c.json({ ok: true });
+	});
