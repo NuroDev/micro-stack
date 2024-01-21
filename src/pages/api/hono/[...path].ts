56155fa0bdae9611c7b0328bc8caedc495e3256a
nurodev <ben@nuro.dev>
2024-01-21 14:04:26 +0000

    Added Hono route logging when in dev mode

--- src/pages/api/hono/[...path].ts
+++ src/pages/api/hono/[...path].ts
@@ -1,4 +1,5 @@
 import { Hono } from 'hono';
+import { showRoutes } from 'hono/dev';
 
 import { commentsHandler } from '~/utils/hono/comments';
 
@@ -11,5 +12,7 @@ type Bindings = Prettify<App.Locals>;
 const app = new Hono<{ Bindings: Bindings }>().basePath('/api/hono/').route('/', commentsHandler);
 
 export type App = typeof app;
+
+if (import.meta.env.DEV) console.log(showRoutes(app));
 
 export const ALL: APIRoute = (context) => app.fetch(context.request, context.locals);
