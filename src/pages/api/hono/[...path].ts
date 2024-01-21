50a4330a5f8773f025ac3b0a2f724c15f637b9d1
nurodev <ben@nuro.dev>
2024-01-21 03:13:50 +0000

    Added `/comments` `POST` endpoint to seed data

--- src/pages/api/hono/[...path].ts
+++ src/pages/api/hono/[...path].ts
@@ -1,4 +1,5 @@
 import { Hono } from 'hono';
+import { faker } from '@faker-js/faker';
 
 import type { APIRoute } from 'astro';
 
@@ -10,7 +11,26 @@ const app = new Hono<{ Bindings: Bindings }>()
 	.basePath('/api/hono/')
 	.get('/comments', async (c) =>
 		c.json({ comments: await c.env.drizzle.query.comments.findMany() }),
-	);
+	)
+	.post('/comments', async (c) => {
+		const rawCount = c.req.query('count');
+		if (!rawCount) return c.json({ ok: false, error: 'Missing count' });
+
+		const count = parseInt(rawCount);
+
+		await Promise.all(
+			new Array(count).fill(null).map(async () => {
+				await c.env.drizzle.insert(c.env.schemas.comments).values({
+					author: faker.person.fullName(),
+					content: faker.lorem.sentence(),
+				});
+
+				await new Promise((resolve) => setTimeout(resolve, Math.random() * 1000));
+			}),
+		);
+
+		return c.json({ ok: true });
+	});
 
 export type App = typeof app;
 
