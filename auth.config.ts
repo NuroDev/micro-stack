431f692662b9f50ed9c8102ea95f4c257aed6b33
nurodev <ben@nuro.dev>
2024-01-23 23:35:04 +0000

    Added GitHub auth provider

--- auth.config.ts
+++ auth.config.ts
@@ -1,10 +1,16 @@
-import { DrizzleAdapter } from '@auth/drizzle-adapter';
 import { defineConfig } from 'auth-astro';
+import { DrizzleAdapter } from '@auth/drizzle-adapter';
+import GitHub from '@auth/core/providers/github';
 
 import { db } from '~/database/client';
 
 export default defineConfig({
 	adapter: DrizzleAdapter(db),
-	providers: [],
+	providers: [
+		GitHub({
+			clientId: import.meta.env.AUTH_GITHUB_CLIENT_ID,
+			clientSecret: import.meta.env.AUTH_GITHUB_CLIENT_SECRET,
+		}),
+	],
 	debug: import.meta.env.DEV,
 });
--- src/env.d.ts
+++ src/env.d.ts
@@ -1,7 +1,10 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
 
-interface ImportMetaEnv extends Readonly<Record<string, string>> {}
+interface ImportMetaEnv extends Readonly<Record<string, string>> {
+	readonly AUTH_GITHUB_CLIENT_ID: string;
+	readonly AUTH_GITHUB_CLIENT_SECRET: string;
+}
 
 interface ImportMeta {
 	readonly env: ImportMetaEnv;
