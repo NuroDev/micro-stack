cb6a720d3e2c5f9adc98031a76a3e7d5e76239f9
nurodev <ben@nuro.dev>
2024-01-23 23:38:13 +0000

    Updated auth config

--- auth.config.ts
+++ auth.config.ts
@@ -6,11 +6,13 @@ import { db } from '~/database/client';
 
 export default defineConfig({
 	adapter: DrizzleAdapter(db),
+	debug: import.meta.env.DEV,
 	providers: [
 		GitHub({
 			clientId: import.meta.env.AUTH_GITHUB_CLIENT_ID,
 			clientSecret: import.meta.env.AUTH_GITHUB_CLIENT_SECRET,
 		}),
 	],
-	debug: import.meta.env.DEV,
+	secret: import.meta.env.AUTH_SECRET,
+	trustHost: Boolean(import.meta.env.AUTH_TRUST_HOST),
 });
