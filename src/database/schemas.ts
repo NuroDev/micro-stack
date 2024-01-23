1fa4c8e489aeffb3f3893815bd9808f53d063048
nurodev <ben@nuro.dev>
2024-01-23 23:06:33 +0000

    Updated Drizzle schemas to use `nanoid` for default ID’s

--- src/database/schemas.ts
+++ src/database/schemas.ts
@@ -1,4 +1,5 @@
 import { text, sqliteTable, integer, primaryKey } from 'drizzle-orm/sqlite-core';
+import { nanoid } from 'nanoid';
 
 import type { AdapterAccount } from '@auth/core/adapters';
 
@@ -29,7 +30,7 @@ export const accounts = sqliteTable(
 export const comments = sqliteTable('comments', {
 	author: text('author').notNull(),
 	content: text('content').notNull(),
-	id: integer('id').primaryKey(),
+	id: text('id').notNull().primaryKey().$defaultFn(nanoid),
 });
 
 export const sessions = sqliteTable('session', {
@@ -43,7 +44,7 @@ export const sessions = sqliteTable('session', {
 export const users = sqliteTable('user', {
 	email: text('email').notNull(),
 	emailVerified: integer('emailVerified', { mode: 'timestamp_ms' }),
-	id: text('id').notNull().primaryKey(),
+	id: text('id').notNull().primaryKey().$defaultFn(nanoid),
 	image: text('image'),
 	name: text('name'),
 });
