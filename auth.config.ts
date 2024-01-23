24b102a2dc03afd70a37b6c71ecc6640c2e8a5cc
nurodev <ben@nuro.dev>
2024-01-23 20:33:20 +0000

    Added initial GitHub provider

--- auth.config.ts
+++ auth.config.ts
@@ -1,8 +1,15 @@
 import { defineConfig } from 'auth-astro';
 
+import GitHub from '@auth/core/providers/github';
+
 export default defineConfig({
 	debug: import.meta.env.DEV,
-	providers: [],
+	providers: [
+		GitHub({
+			clientId: import.meta.env.AUTH_GITHUB_CLIENT_ID,
+			clientSecret: import.meta.env.AUTH_GITHUB_CLIENT_SECRET,
+		}),
+	],
 	secret: import.meta.env.AUTH_SECRET,
 	trustHost: Boolean(import.meta.env.AUTH_TRUST_HOST),
 });
