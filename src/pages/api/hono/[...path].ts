8454d42ad862a9f59cc566644a8ae0eb8369c8f1
nurodev <ben@nuro.dev>
2024-01-23 00:01:08 +0000

    Added `secureHeaders` middleware to Hono api

--- src/pages/api/hono/[...path].ts
+++ src/pages/api/hono/[...path].ts
@@ -1,10 +1,13 @@
 import { Hono } from 'hono';
+import { secureHeaders } from 'hono/secure-headers';
+
+import { db } from '~/database/client';
 
 import type { APIRoute } from 'astro';
-import { db } from '~/database/client';
 
 const app = new Hono()
 	.basePath('/api/hono/')
+	.use('*', secureHeaders())
 	.get('/comments', async (c) => c.json({ comments: await db.query.comments.findMany() }));
 
 export type App = typeof app;
