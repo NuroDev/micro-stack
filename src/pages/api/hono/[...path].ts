84277f06948aa2f33c7caeffcdca449fc9438ee0
nurodev <ben@nuro.dev>
2024-01-24 18:48:37 +0000

    Removed Hono `showRoutes`

--- src/pages/api/hono/[...path].ts
+++ src/pages/api/hono/[...path].ts
@@ -1,6 +1,5 @@
 import { Hono } from 'hono';
 import { secureHeaders } from 'hono/secure-headers';
-import { showRoutes } from 'hono/dev';
 
 import { commentsHandler } from '~/api/comments';
 
@@ -21,7 +20,5 @@ const app = new Hono<{ Bindings: BaseBindings }>()
 	.route('/', commentsHandler);
 
 export type App = typeof app;
-
-if (import.meta.env.DEV) console.log(showRoutes(app));
 
 export const ALL: APIRoute = (context) => app.fetch(context.request, context.locals);
